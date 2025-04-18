45257ce84045d1eb53da7c5f4ce9b8024fdf1482
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-04-18 13:59:30 +0200

    back button

--- app/src/main/java/com/example/quiztory/History2.kt
+++ app/src/main/java/com/example/quiztory/History2.kt
@@ -1,11 +1,14 @@
 package com.example.quiztory
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +21,18 @@ class History2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-         
+            Column {
+                Text(text="page 2 ")
+                Button(onClick = {
+                    val back = Intent(this@History2 ,MainActivity::class.java)
+                    startActivity(back)
+
+                })
+                { Text(text="back")
+                }
+
+            }
+
         }
     }
 }
