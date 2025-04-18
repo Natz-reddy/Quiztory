626406d7340bd6c6cda4f0ee6005ac29ca32b4ec
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-04-18 15:40:22 +0200

    editing welcome screen layout

--- app/src/main/java/com/example/quiztory/MainActivity.kt
+++ app/src/main/java/com/example/quiztory/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +39,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Surface(modifier = Modifier.fillMaxSize(),
-                color= Color(0xFFC4B37B))
+                color= Color(0xFF265BC5)
+            )
             {
                 Column (
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -50,21 +52,31 @@ class MainActivity : ComponentActivity() {
                 )
 
                 {
-                    Spacer(modifier = Modifier.height(30.dp))
-                    
-                    Text(text = "WELCOME",
-                        fontSize = 45.sp,// font size
+                    Spacer(modifier = Modifier.height(200.dp))
+
+                    Text(text = "QUIZTORY",
+                        fontSize = 45.sp,
                         fontWeight = FontWeight.ExtraBold,
                         fontFamily = FontFamily.SansSerif,
-                        color = Color.Yellow)
+                        color = Color.Yellow,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Text(text=" Put your knowledge to the test. Answer five history questions , " +
+                            "True/ False about the world history events and figures",
+                        fontSize = 35.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily.SansSerif,
+                        color = Color.White,
+                        textAlign = TextAlign.Center)
 
-                    Spacer(modifier = Modifier.height(600.dp))
+                    Spacer(modifier = Modifier.height(100.dp))
 
                     Button(onClick = {
                         val start = Intent(this@MainActivity, History2::class.java)
                         startActivity(start)},
                         colors = ButtonDefaults.buttonColors(
-                            containerColor =Color(0x68FF0026),
+                            containerColor =Color(0xFFE80B0B),
                             contentColor = Color.White
                         ),
                         modifier = Modifier.padding(bottom = 100.dp)
