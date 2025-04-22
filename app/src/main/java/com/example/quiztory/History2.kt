e457ead7d75659e7ebddee365fbb4a09028dc94c
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-04-22 17:59:54 +0200

    beginning the new page
    I started to get the layout done and place everything in position

--- app/src/main/java/com/example/quiztory/History2.kt
+++ app/src/main/java/com/example/quiztory/History2.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quiztory.ui.theme.QuiztoryTheme
 
 class History2 : ComponentActivity() {
@@ -21,18 +32,32 @@ class History2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column {
-                Text(text="page 2 ")
-                Button(onClick = {
-                    val back = Intent(this@History2 ,MainActivity::class.java)
-                    startActivity(back)
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color(0xFFF5F5DC)
+            )
+            {
+                Column ( horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
 
-                })
-                { Text(text="back")
+                )
+                {
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(text="Choose a genre",
+                        fontSize = 55.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily.SansSerif,
+                        color = Color(0xFFE7CC6A),
+                        textAlign = TextAlign.Center
+                    )
+
+
                 }
-
-            }
-
+                }
         }
     }
 }
