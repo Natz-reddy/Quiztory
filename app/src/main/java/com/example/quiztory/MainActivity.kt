04c373b335ddd4682fe8a97b0b5f8e6893f2fa39
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-04-22 16:47:00 +0200

    editing layout of text

--- app/src/main/java/com/example/quiztory/MainActivity.kt
+++ app/src/main/java/com/example/quiztory/MainActivity.kt
@@ -80,7 +80,9 @@ class MainActivity : ComponentActivity() {
 
                     OutlinedTextField(value = description, onValueChange ={ description=it},
                         placeholder = { Text(text=" Put your knowledge to the test. Answer five history questions , " +
-                                "True/ False about the world history events and figures", color = Color.Black) },
+                                "True/ False about the world history events and figures",
+                            color = Color.Black,
+                            textAlign = TextAlign.Center)  },
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(150.dp)
