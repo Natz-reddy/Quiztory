481af418864a20a555c585dc6ccbba8c34a91719
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-04-30 11:17:01 +0200

    i changed the if statement to something easier

--- app/src/main/java/com/example/quiztory/History2.kt
+++ app/src/main/java/com/example/quiztory/History2.kt
@@ -71,9 +71,20 @@ class History2 : ComponentActivity() {
 
 
 
-        Spacer(modifier = Modifier.height(20.dp))
+            if (!scorescreen) {
 
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground1),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.fillMaxSize().alpha(0.9f)
+                    )
+                }
 
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Transparent
                 )
                 {
                     Text(
