d463c002fffbacd3ae7eb8db683ff7a42b5b719f
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-05-15 22:57:34 +0200

    editing page layout , deleting old code

--- app/src/main/java/com/example/quiztory/Score.kt
+++ app/src/main/java/com/example/quiztory/Score.kt
@@ -9,21 +9,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +48,8 @@ class Score : ComponentActivity() {
 
             val score = intent.getIntExtra("score", 0)
             val total = intent.getIntExtra("total", 5)
+            val answers = intent.getBooleanArrayExtra("answers")?: BooleanArray(0)
+            var showAnswers by remember { mutableStateOf(false) }
 
             Box(modifier = Modifier.fillMaxSize()) {
                 Image(
@@ -82,12 +94,21 @@ class Score : ComponentActivity() {
 Spacer(modifier = Modifier.height(30.dp))
 
                     Button(onClick = {
+                        showAnswers = !showAnswers
 
                     }) {
-                        Text(text = "review answers")
+                        Text(text = if (showAnswers)"hide answers" else "review answers")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        if (showAnswers) {
+
+                        }
                     }
 
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(24.dp))
 
                     Button(onClick = {
                         val playagain = Intent(this@Score, History2::class.java)
